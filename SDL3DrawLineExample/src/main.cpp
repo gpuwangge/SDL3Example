7de399c73b9c46ada741af1ea04a7d06518960f3
Connor <wxj995@gmail.com>
2024-10-13 16:42:53 -0700

    change example code from pen to mouse

--- SDL3DrawLineExample/src/main.cpp
+++ SDL3DrawLineExample/src/main.cpp
@@ -2,18 +2,23 @@
 #define SDL_MAIN_USE_CALLBACKS 1  /* use the callbacks instead of main() */
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
+#include <iostream>
+/*
+* Modified the code from using pen to mouse, to draw lines in the window
+*/
+
 
 /* We will use this renderer to draw into this window every frame. */
 static SDL_Window *window = NULL;
 static SDL_Renderer *renderer = NULL;
 static SDL_Texture *render_target = NULL;
-static float pressure = 0.0f;
+static bool bMouseDown = false;
 static float previous_touch_x = -1.0f;
 static float previous_touch_y = -1.0f;
 
 /* This function runs once at startup. */
-SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
-{
+SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]){
+    //std::cout<<"SDL_AppInit"<<std::endl;
     SDL_SetAppMetadata("Example Pen Drawing Lines", "1.0", "com.example.pen-drawing-lines");
 
     if (!SDL_Init(SDL_INIT_VIDEO)) {
@@ -36,7 +41,7 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
 
     /* just blank the render target to gray to start. */
     SDL_SetRenderTarget(renderer, render_target);
-    SDL_SetRenderDrawColor(renderer, 100, 100, 100, 255);
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
     SDL_RenderClear(renderer);
     SDL_SetRenderTarget(renderer, NULL);
     SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
@@ -47,38 +52,39 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
 /* This function runs when a new event (mouse input, keypresses, etc) occurs. */
 SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
 {
+    //std::cout<<"SDL_AppEvent, event->type = "<< event->type <<std::endl;
     if (event->type == SDL_EVENT_QUIT) {
+        std::cout<<"SDL_EVENT_QUIT"<<std::endl;
         return SDL_APP_SUCCESS;  /* end the program, reporting success to the OS. */
     }
 
-    /* There are several events that track the specific stages of pen activity,
-       but we're only going to look for motion and pressure, for simplicity. */
-    if (event->type == SDL_EVENT_PEN_MOTION) {
-        /* you can check for when the pen is touching, but if pressure > 0.0f, it's definitely touching! */
-        if (pressure > 0.0f) {
-            if (previous_touch_x >= 0.0f) {  /* only draw if we're moving while touching */
-                /* draw with the alpha set to the pressure, so you effectively get a fainter line for lighter presses. */
-                SDL_SetRenderTarget(renderer, render_target);
-                SDL_SetRenderDrawColorFloat(renderer, 0, 0, 0, pressure);
-                SDL_RenderLine(renderer, previous_touch_x, previous_touch_y, event->pmotion.x, event->pmotion.y);
-            }
-            previous_touch_x = event->pmotion.x;
-            previous_touch_y = event->pmotion.y;
-        } else {
-            previous_touch_x = previous_touch_y = -1.0f;
-        }
-    } else if (event->type == SDL_EVENT_PEN_AXIS) {
-        if (event->paxis.axis == SDL_PEN_AXIS_PRESSURE) {
-            pressure = event->paxis.value;  /* remember new pressure for later draws. */
-        }
+    if (event->type == SDL_EVENT_MOUSE_BUTTON_DOWN) {
+        bMouseDown = true;
     }
+
+    if (event->type == SDL_EVENT_MOUSE_BUTTON_UP) {
+        bMouseDown = false;
+    }
+
+
+    if (event->type == SDL_EVENT_MOUSE_MOTION) {
+        //std::cout<<"SDL_EVENT_MOUSE_MOTION"<<std::endl;
+        if(bMouseDown){
+            SDL_SetRenderTarget(renderer, render_target);
+            SDL_SetRenderDrawColorFloat(renderer, 0, 0, 0, 255);
+            SDL_RenderLine(renderer, previous_touch_x, previous_touch_y, event->pmotion.x, event->pmotion.y);
+        }
+
+        previous_touch_x = event->pmotion.x;
+        previous_touch_y = event->pmotion.y;
+    } 
 
     return SDL_APP_CONTINUE;  /* carry on with the program! */
 }
 
 /* This function runs once per frame, and is the heart of the program. */
-SDL_AppResult SDL_AppIterate(void *appstate)
-{
+SDL_AppResult SDL_AppIterate(void *appstate){
+    //std::cout<<"SDL_AppIterate"<<std::endl;
     /* make sure we're drawing to the window and not the render target */
     SDL_SetRenderTarget(renderer, NULL);
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
@@ -89,8 +95,8 @@ SDL_AppResult SDL_AppIterate(void *appstate)
 }
 
 /* This function runs once at shutdown. */
-void SDL_AppQuit(void *appstate, SDL_AppResult result)
-{
+void SDL_AppQuit(void *appstate, SDL_AppResult result){
+    std::cout<<"SDL_AppQuit"<<std::endl;
     SDL_DestroyTexture(render_target);
     /* SDL will clean up the window/renderer for us. */
 }
